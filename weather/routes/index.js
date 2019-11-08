e68bae0569978f0de66f011f56ca725a174fbdef
wguardado <wilbert.guardado@gmail.com>
2019-11-08 13:18:38 -0500

    Fix sintax error

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fetch = require("node-fetch");
 require('dotenv').config();
-const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+const OWM_API_KEY = process.env.   || 'invalid_key';
 const UNITS = process.env.UNITS || 'metric';
 
 /* GET home page. */
